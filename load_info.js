9e5658f39a9283b3b4f991fa30b44fb26279edef
IrynaPap <papiryna@gmail.com>
2016-08-11 23:55:55 +0300

    remove unused logs

--- load_info.js
+++ load_info.js
@@ -1,21 +1,20 @@
+loadUsers()
 
 function loadUsers() {
     var request = new XMLHttpRequest();
     request.open('GET', 'https://api.github.com/users', true); /* Третий аргумент true означает асинхронность */
     request.onreadystatechange = function () {
         if (request.readyState == 4) {
-            if (request.status == 200)
-                console.log(request.responseText);
+            if (request.status == 200) {
+                var user = JSON.parse(request.responseText);
+                createTable(user);
+            }
             else
                 console.log("Error loading page\n");
-            var user = JSON.parse(request.responseText);
-            //Downloading users
-            createTable(user);
         }
     };
     request.send(null);
 }
-console.log(loadUsers());
 
 
 var getInfo = function (login, setInfo) {
@@ -23,16 +22,15 @@ var getInfo = function (login, setInfo) {
     request.open('GET', 'https://api.github.com/users/' + login, true); /* Третий аргумент true означает асинхронность */
     request.onreadystatechange = function () {
         if (request.readyState == 4) {
-            if (request.status == 200)
-                console.log(request.responseText + "vtoroy");
+            if (request.status == 200) {
+                var userUnit = JSON.parse(request.responseText);
+                setInfo(userUnit)
+            }
             else
                 console.log("Error loading page\n");
-            var userUnit = JSON.parse(request.responseText);
-            setInfo(userUnit)
         }
     };
     request.send(null);
-
 };
 
 
@@ -43,16 +41,14 @@ var getInfoUserLinks = function getInfoUserLinks(login, link, userLinks) {
     request.open('GET', 'https://api.github.com/users/' + login + '/' + link, true); /* Третий аргумент true означает асинхронность */
     request.onreadystatechange = function () {
         if (request.readyState == 4) {
-            if (request.status == 200)
-                console.log(request.responseText + " podpischiki");
+            if (request.status == 200) {
+                var follow = JSON.parse(request.responseText);
+                var count = follow.length;
+                userLinks(count);
+            }
             else
                 console.log("Error loading page\n");
-            var follow = JSON.parse(request.responseText);
-            var count = follow.length;
-            userLinks(count);
         }
     };
     request.send(null);
-
 };
-      /*  loadLinks(userUnit,getInfoUserLinks);*/
